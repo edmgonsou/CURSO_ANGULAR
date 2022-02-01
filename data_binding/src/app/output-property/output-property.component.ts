b86e92113e4e59d5f8320a8d4d3fda34eb860460
edmgonsou <gonsou23@hotmail.com>
2022-02-01 19:55:02 -0300

    Aulas 15 e 16

--- data_binding/src/app/output-property/output-property.component.ts
+++ data_binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -11,14 +11,16 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor: number = 0;
 
   @Output() mudouValor = new EventEmitter();
+
+  @ViewChild('campoInput') campoValorInput!: ElementRef;
   
   incrementa(){
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa(){
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
